438540e71cdfd80ff4163ba9e65cb47ffa88cf50
Haad majeed <haadmajeedch27@gmail.com>
2025-01-03 10:17:37 +0500

    pratice

---
+++ practice/-3.cpp
@@ -0,0 +1,20 @@
+// C++ Program starts here
+#include <iostream>
+using namespace std;
+    
+    
+int main()
+{
+    int *ptr= new int [1000000000000];
+
+    ptr[112]=120;
+
+    cout<<*(ptr+112)<<endl;
+
+    delete[] ptr;
+
+
+    
+     return 0;
+    
+}
